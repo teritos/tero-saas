89b06785be647a547192686f3ad7d795599b42ac
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2016-07-30 16:39:55 -0300

    tests pass! :)

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -41,13 +41,40 @@ class AlarmProfile(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    alarm = models.OneToOneField(AlarmProfile)
+    alarm = models.ForeignKey(AlarmProfile)
     telegram = models.OneToOneField(
         TelegramProfile,
         null=True,
         blank=True, 
         on_delete=models.CASCADE
         )
+
+    @classmethod
+    def create(cls, username, password, ftpd=True, alarm_profile=None, telegram_user=True, **kwargs):
+        """Create a basic user."""
+        from ftpd.models import FTPUser
+        user = User.objects.create(username=username, password=password)
+
+        if ftpd and not FTPUser.objects.filter(user=user).exists():
+            from ftpd.models import FTPUser
+            FTPUser.objects.create(user=user)
+
+        if not alarm_profile:
+            alarm_profile = AlarmProfile.objects.create()
+
+        if telegram_user:
+            telegram_token = kwargs['telegram_token']
+            telegram_chat_id = kwargs['telegram_chat_id']
+            bot = TelegramProfile.objects.create(
+                bot_token=telegram_token, 
+                chat_id=telegram_chat_id)
+
+        user_profile = cls.objects.create(
+            user=user,
+            alarm=alarm_profile,
+            telegram=bot)
+
+        return user_profile
     
     def __str__(self):
         return "{}".format(self.user)
---
+++ app/webapp/tests.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from webapp.models import (
+    TelegramProfile,
+    AlarmProfile,
+    UserProfile
+)
+
+
+class TestUserProfile(TestCase):
+
+    def setUp(self):
+        self.alarm = AlarmProfile.objects.create()
+        self.user = User.objects.create(username='test', password='secret')
+        self.user2 = User.objects.create(username='test2', password='secret')
+        self.user_profile = UserProfile.objects.create(
+            user=self.user, alarm=self.alarm
+            )
+        self.user_profile2 = UserProfile.objects.create(
+            user=self.user2, alarm=self.alarm
+        )
+
+    def test_user_alarm(self):
+        user_profile = self.user_profile
+        user_profile2 = self.user_profile2
+        self.assertTrue(user_profile.alarm.active == False)
+        self.assertTrue(user_profile2.alarm.active == False)
+
+        user_profile.alarm.activate()
+        self.assertTrue(user_profile.alarm.active == True)
+        self.assertTrue(user_profile2.alarm.active == True)
+        
+        user_profile2.alarm.deactivate()
+        self.assertTrue(user_profile.alarm.active == False)
+        self.assertTrue(user_profile2.alarm.active == False)
+
+    def test_user_telegram_bot(self):
+        user_profile = self.user_profile
+        self.assertTrue(user_profile.telegram == None)
+
+        bot = TelegramProfile.objects.create(bot_token='xxx', chat_id='secret')
+        user_profile.telegram = bot
+        user_profile.save()
+        self.assertTrue(user_profile.telegram == bot)
+
+    def test_create_user_profile(self):
+        user_profile = UserProfile.create(
+            username='emi',
+            password='secret',
+            fptd=True,
+            alarm_profile=None,
+            telegram_user=True,
+            telegram_token='xxx',
+            telegram_chat_id='xxx'
+        )
+
+        user = User.objects.get(username='emi')
+        self.assertEquals(user_profile.user, user)
+
+        self.assertTrue(user_profile.alarm.active == False)
