05346e8901687e5687f3fac714ca62219d1d3a33
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2016-10-30 21:06:48 -0300

    saco field username de telegram model

--- tero/notifier/models.py
+++ tero/notifier/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class Telegram(models.Model):
-    pin = models.CharField(max_length=10)
-    username = models.CharField(max_length=50)
+    pin = models.CharField(max_length=20)
 
     def __str__(self):
         return self.pin
