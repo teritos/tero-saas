645abc58a808a4d6c85ceb4418c0e7ac6515e618
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2016-11-17 21:13:55 -0300

    no tenemos mas notifier, juiiiira

--- tero-saas/notifier/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from notifier.models import Telegram
-
-
-admin.site.register(Telegram)
--- tero-saas/notifier/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotifierConfig(AppConfig):
-    name = 'notifier'
