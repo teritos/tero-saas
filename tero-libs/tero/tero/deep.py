208096b83a227c2be791b3a7e481d2a713f0091c
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2016-12-10 23:17:50 -0300

    Seteo directorio de trabajo para la deep neural network

--- tero-libs/tero/tero/deep.py
+++ tero-libs/tero/tero/deep.py
@@ -4,16 +4,17 @@ import shlex
 import os
 
 
-def run_detector(image_path, cfg):
+def run_detector(image_path, cfg=None):
     predictions = []
 
     config = configparser.ConfigParser()
-    config.read(cfg)
+    config.read(cfg or os.getenv('DEEPINI'))
     deepnet = config['DEEPNET']
     bin_    = deepnet['DeepNeuralBinary']
     cfg     = deepnet['Cfg']
     data    = deepnet['Data']
     weights = deepnet['Weights']
+    os.chdir(deepnet['Chdir'])
 
     cmd = '{} detector test {} {} {} {}'.format(bin_, data, cfg, weights, image_path)
     cmd = shlex.split(cmd)
@@ -21,7 +22,4 @@ def run_detector(image_path, cfg):
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE)
     data = completed_process.stdout.split(b'\n')
 
-    pcount, psize = data.split('H%23')[-1].split()
-    import ipdb; ipdb.set_trace()
-
-    return predictions 
+    return data 
