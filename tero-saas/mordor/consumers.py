1ebe8c5580fb150d2dd3c337da8edc58193d3b9b
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2017-01-07 15:39:50 -0300

    agrego logging al consumer de mordor

--- tero-saas/mordor/consumers.py
+++ tero-saas/mordor/consumers.py
@@ -1,25 +1,23 @@
 """Mordor channel consumers."""
 
-from channels import Channel, Group
-from channels.auth import http_session_user, channel_session_user, channel_session_user_from_http
+import logging
 from mordor.channels import get_alarm_group
+from channels.auth import channel_session_user, channel_session_user_from_http
+
+
+LOGGER = logging.getLogger('mordor')
 
 
 @channel_session_user_from_http
 def ws_auth(message):
     """Add user to its group alarm."""
-    if not message.user:
+    if not message.user or message.user.is_anonymous:
+        LOGGER.debug('Invalid user %s', message.user)
         return
     group = get_alarm_group(message.user)
     group.add(message.reply_channel)
+    LOGGER.debug('User %s added to group %s', message.user, group.name)
 
-# Connected to websocket.receive
-@channel_session_user
-def ws_message(message):
-    group = get_alarm_group(message.user)
-    Group("chat-%s" % message.user.username[0]).send({
-        "text": message['text'],
-    })
 
 # Connected to websocket.disconnect
 @channel_session_user
@@ -27,6 +25,7 @@ def ws_disconnect(message):
     """When user disconnects, remove its group name."""
     group = get_alarm_group(message.user)
     group.discard(message.reply_channel)
+    LOGGER.debug('User %s removed from %s', message.user, group.name)
 
 
 def ws_echo(message):
