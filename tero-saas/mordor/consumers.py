36905495f62d12ecbf3cebb2a67fea8f78e84469
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2017-01-07 14:52:13 -0300

    agrego handlers para cuando un usuario se conecta, y desconecta

--- tero-saas/mordor/consumers.py
+++ tero-saas/mordor/consumers.py
@@ -1,5 +1,33 @@
 """Mordor channel consumers."""
 
+from channels import Channel, Group
+from channels.auth import http_session_user, channel_session_user, channel_session_user_from_http
+from mordor.channels import get_alarm_group
+
+
+@channel_session_user_from_http
+def ws_auth(message):
+    """Add user to its group alarm."""
+    if not message.user:
+        return
+    group = get_alarm_group(message.user)
+    group.add(message.reply_channel)
+
+# Connected to websocket.receive
+@channel_session_user
+def ws_message(message):
+    group = get_alarm_group(message.user)
+    Group("chat-%s" % message.user.username[0]).send({
+        "text": message['text'],
+    })
+
+# Connected to websocket.disconnect
+@channel_session_user
+def ws_disconnect(message):
+    """When user disconnects, remove its group name."""
+    group = get_alarm_group(message.user)
+    group.discard(message.reply_channel)
+
 
 def ws_echo(message):
     """Echo WSGI messages."""
