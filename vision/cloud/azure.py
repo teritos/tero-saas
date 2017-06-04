a7361c2c9b22d0f31b67b0b411e074e1cabcd94b
Emiliano Dalla Verde Marcozzi <emiliano@devsar.com>
2017-06-03 21:03:05 -0300

    creo metodo helper que nos dice si encuentra tags relacionados con personas en la imagen

--- vision/cloud/azure.py
+++ vision/cloud/azure.py
@@ -1,8 +1,24 @@
 import os
+import logging
 import requests
 
-AZURE_API_ENDPOINT = os.getenv('AZURE_CLOUD_VISION_ENDPOINT')
-AZURE_ACCESS_KEY1 = os.getenv('AZURE_CLOUD_VISION_KEY1')
+AZURE_API_ENDPOINT = os.getenv('AZURE_API_ENDPOINT')
+AZURE_ACCESS_KEY1 = os.getenv('AZURE_ACCESS_KEY1')
+logger = logging.getLogger('vision')
+
+
+def find_humans_on(image):
+    """Find humans on image."""
+    found_tags = []
+    positive_tags = ['man', 'person', 'woman', 'people', 'young']
+    metadata = get_image_tags(image)
+    if not metadata:
+        return found_tags
+    tags = metadata['description']['tags']
+    for tag in tags:
+        if tag in positive_tags:
+            found_tags.append(tag)
+    return found_tags
 
 
 def get_image_tags(image):
@@ -18,5 +34,7 @@ def get_image_tags(image):
     try:
         response = requests.post(url, params=params, data=image, headers=headers)
         data = response.json()
+    except Exception as exc:
+        logger.exception('Error en Azure Vision API.')
     finally:
         return data
